fc931cf8c5a729c1c3383fe2ae37f12924768d59
pitsmit <petr_smirnov_2004@mail.ru>
2025-10-20 19:16:50 +0300

    babbel

--- Server/babel.config.js
+++ Server/babel.config.js
@@ -13,6 +13,8 @@ module.exports = {
     plugins: [
         ["@babel/plugin-proposal-decorators", { "legacy": true }],
         ["@babel/plugin-proposal-class-properties", { "loose": true }],
+        ["@babel/plugin-transform-private-methods", { "loose": true }],
+        ["@babel/plugin-transform-private-property-in-object", { "loose": true }],
 
         "babel-plugin-transform-typescript-metadata",
 
