6e45b2a782e7c5a1615aebe4a68498d707668424
pitsmit <petr_smirnov_2004@mail.ru>
2025-10-21 13:33:51 +0300

    no trash

--- Server/dist/Facade/container.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-
-Object.defineProperty(exports, "__esModule", {
-  value: true
-});
-exports.container = void 0;
-require("reflect-metadata");
-var _inversify = require("inversify");
-const container = exports.container = new _inversify.Container();
