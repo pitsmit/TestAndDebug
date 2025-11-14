49969be3e69e95b564e390b986999c55fe8101a6
pitsmit <petr_smirnov_2004@mail.ru>
2025-11-14 21:55:58 +0300

    'fd15

--- Server/framework-express/config.js
+++ Server/framework-express/config.js
@@ -1,10 +1,12 @@
 const path = require('path');
 
+const commonPath = path.join(__dirname, '..', 'common');
+
 const config = {
   URL_PORT: 3000,
   URL_PATH: 'http://localhost',
   BASE_VERSION: '',
-  OPENAPI_YAML: path.join(__dirname, '..', 'common', 'api', 'openapi.yaml'),
+  OPENAPI_YAML: path.join('.', commonPath, 'api', 'openapi.yaml'),
 };
 
 module.exports = config;
